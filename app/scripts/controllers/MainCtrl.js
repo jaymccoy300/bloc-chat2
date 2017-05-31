b5490bf2e7de3edd4b1007c0d77256825be7c9d3
jay <jaymccoy300@gmail.com>
2017-05-31 14:38:02 -0700

    Checkpoint 4, fix

--- app/scripts/controllers/MainCtrl.js
+++ app/scripts/controllers/MainCtrl.js
@@ -1,6 +1,8 @@
 (function() {
-    function MainCtrl(Room, $uibModal) {
+    function MainCtrl(Room, Message, $uibModal) {
         this.chatRoomArray = Room.all;
+        this.currentRoom = null;
+        
         
         this.addRoom = function() {
             $uibModal.open({
@@ -8,14 +10,18 @@
                 size: 'sm',
                 controller: 'ModalCtrl',
                 controllerAs : 'modal'
-                
-
-                
+                               
             });
-        }      
+        }
+        
+        this.setCurrentRoom = function(room) {
+            this.currentRoom = room;
+            this.messages = Message.getByRoomId(this.currentRoom.$id);
+            console.log(this.messages);
+        }
     }
 
     angular
         .module('blocChat')
-        .controller('MainCtrl', ['Room','$uibModal', MainCtrl]);
+        .controller('MainCtrl', ['Room', 'Message', '$uibModal', MainCtrl]);
 })();
